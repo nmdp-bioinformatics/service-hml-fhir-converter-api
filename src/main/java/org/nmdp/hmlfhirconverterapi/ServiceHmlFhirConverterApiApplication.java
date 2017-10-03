23e009caa7a42aefcdf535b59f1e677b252e85e1
Andrew S. Brown, Ph.D <abrown3@nmdp.org>
2017-10-03 08:39:29 -0500

    Updates

--- src/main/java/org/nmdp/hmlfhirconverterapi/ServiceHmlFhirConverterApiApplication.java
+++ src/main/java/org/nmdp/hmlfhirconverterapi/ServiceHmlFhirConverterApiApplication.java
@@ -36,7 +36,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
 @SpringBootApplication
-@ComponentScan({"org.nmdp.kafkaproducer", "io.swagger.api", "org.nmdp.hmlfhirconverterapi"})
+@ComponentScan({"org.nmdp.kafkaproducer", "io.swagger.api", "org.nmdp.hmlfhirconverterapi", "org.nmdp.fhirsubmission"})
 public class ServiceHmlFhirConverterApiApplication {
 
     public static void main(String[] args) {
